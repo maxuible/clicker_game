473148fb026525b3ec9c2691a17c69c8b8f5e24f
unknown <max@fakemail.net>
2022-10-06 15:47:15 -0500

    resetting

--- addClickAddAmtNode.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var clickAddAmt = 1
-
-func _on_addClickAddAmtBtn_pressed():
-	if(get_node("..").total >= 5):
-		clickAddAmt += 1
-		get_node("..").total -= 5
-		get_node("..").updateDisplay()
--- mainClickerNode.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-func _on_mainClickerBtn_pressed():
-	get_node("..").total += get_node("../addClickAddAmtNode").clickAddAmt
-	get_node("..").updateDisplay()
